8d97514c82fdaa7ef08d7548de87cfabb7251ee3
Samuel-de-Oliveira <samwolfg13@gmail.com>
2022-02-03 12:43:52 -0300

    add

--- main.cpp
+++ main.cpp
@@ -15,16 +15,17 @@ int main(int argc, char** argv){
 		"_*_ List of comands _*_\n"
        		"\n"
 		"rectangle: Calculate the area of a rectangle (b x h) [easarea rectangle [\"b\" value here] [\"h\" value here]]\n"
-		"square: Calculate the area of a square (s²) [easarea square [s value here]]"
+		"square: Calculate the area of a square (s²) [easarea square [s value here]]\n"
 		"triangle: Calculate the area of a tringle ((b x h) / 2) [easarea triangle [\"b\" value here] [\"h\" value here]]\n"
-		"circle: calculate the area of a circle (2πr²) [easarea circle [\"r\" value here]]\n";
+		"circle: Calculate the area of a circle (2πr²) [easarea circle [\"r\" value here]]\n"
+		"trapezium: Calculate the area of a trapezium ((B + b) * h / 2) [easarea trapezium [\"B\" value here] [\"b\" value here] [\"h\" value here]]\n";
 	}
 
 	if (check == "rectangle"){ std::cout << parseFloat(argv[2]) * parseFloat(argv[3]) << "\n";}
 	if (check == "square"){ std::cout << pow(parseFloat(argv[2]), 2) << "\n";  }
 	if (check == "triangle"){ std::cout << (parseFloat(argv[2]) * parseFloat(argv[3])) / 2 << "\n"; }
 	if (check == "circle"){ std::cout << 2 * Pi * pow(parseFloat(argv[2]), 2) << "\n"; }
-	if (check == "")
+	if (check == "trapezium"){ std::cout << (parseFloat(argv[2]) + parseFloat(argv[3])) * parseFloat(argv[4]) / 2 << "\n"; }
 
 	return 0;
 }
