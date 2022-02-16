3fdada0c0bd1b3544399167413716029099f3d4f
Samuel-de-Oliveira <samwolfg13@gmail.com>
2022-02-16 14:35:22 -0300

    add

--- lib.h
+++ lib.h
@@ -2,7 +2,7 @@
 using namespace std;
 
 const float Pi = 3.1415926535897; // I conseidered this value for π
-const string Version = "0.1 Beta.";
+const string Version = "0.2 Beta."; // Version's number
 
 int parseInt(const string &str) {
 
