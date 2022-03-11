7eaa2d98584161ac83ef387fbbcc0de3ebb2f463
Samuel-de-Oliveira <samwolfg13@gmail.com>
2022-03-11 09:48:40 -0300

    Add new funtions

--- lib.h
+++ lib.h
@@ -1,8 +1,17 @@
+/*
+ * # -*- Easarea lib -*- #
+ *
+ * Here a have created some
+ * funtions to use in program,
+ * enjoy it anyway.
+ *
+ */
+
 #include <sstream>
 using namespace std;
 
 const float Pi = 3.1415926535897; // I conseidered this value for π
-const string Version = "0.2 Beta."; // Version's number
+const string Version = "0.1.1 Beta."; // Version's number
 
 int parseInt(const string &str) {
 
@@ -24,7 +33,10 @@ float parseFloat(const string &str) {
 
 float circleParts(float angle) {
 	
-	if (angle > 360){ return 1; }
+	if (angle > 360){ 
+		cout << "The angle can't be a value higher than 360.\nPlease, digit a valid value.\n";
+		return 0;
+	}
 	else { return 360 / angle; }
 
 }
--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+/*
+ *  # -*- Easarea's source code -*- #
+ *
+ *  You can use this code to do any
+ *  thing you want, you are free to
+ *  do it.
+ *
+ *  Crated by: Samuel de Oliveira (Samuel-de-Oliviera)
+ *  Code contribuitors: None
+ *
+ */
+
 #include <iostream>
 #include "lib.h"
 #include <string>
@@ -6,7 +18,8 @@
 int main(int argc, char** argv){
 	
 	string check(argv[1]);
-			
+	
+	// Help message
 	if (check == "help"){
 		std::cout <<
 		"To use you need to follow this format:\n"
@@ -20,12 +33,15 @@ int main(int argc, char** argv){
 		"circle: Calculate the area of a circle (πr²) [easarea circle [\"r\" value here]]\n"
 		"trapezium: Calculate the area of a trapezium ((B + b) * h / 2) [easarea trapezium [\"B\" value here] [\"b\" value here] [\"h\" value here]]\n"
 		"diamond: Calculate the area of a diamond ((D * d) / 2) [easarea diamond [\"D\" value here] [\"b\" value here]]\n"
-		"hypotenuse: Calculate the hypotenuse of a triangle (X = Y² + Z²) [easarea hypotenuse [\"Y\" value here] [\"Z\" value here]]\n"
+		"hypotenuse: Calculate the hypotenuse of a triangle (X² = Y² + Z²) [easarea hypotenuse [\"Y\" value here] [\"Z\" value here]]\n"
 		"regular_polygon: Calculate the area of a regular polygon ((P * A) / 2) [easarea regular_polygon [\"P\" value here] [\"A value here\"]]\n"
+		"cube: Calculate the volume of a cube (s³) [easarea cube [\"s\" value here]]\n"
+		"parallelepiped: Calculate the valume of a parallelepiped (b * h * w) [easarea parallelepiped [\"b\" value here] [\"h\" value here] [\"w\" value here]]\n"
 
 		"version: Show the version of program [easarea version]\n"
 		"help: Show this message [easarea help]\n";}
 
+	// shape calculators
 	if (check == "rectangle"){ std::cout << parseFloat(argv[2]) * parseFloat(argv[3]) << "\n";}
 	if (check == "square"){ std::cout << pow(parseFloat(argv[2]), 2) << "\n";  }
 	if (check == "triangle" || check == "diamond"){ std::cout << (parseFloat(argv[2]) * parseFloat(argv[3])) / 2 << "\n"; }
@@ -33,7 +49,10 @@ int main(int argc, char** argv){
 	if (check == "trapezium"){ std::cout << (parseFloat(argv[2]) + parseFloat(argv[3])) * parseFloat(argv[4]) / 2 << "\n"; }
 	if (check == "hypotenuse"){ std::cout << hypot(parseFloat(argv[2]), parseFloat(argv[3])) << "\n"; }
 	if (check == "regular_polygon"){ std::cout << (parseFloat(argv[2]) * parseFloat(argv[3])) / 2 << "\n"; }
+	if (check == "cube"){ std::cout << pow(parseFloat(argv[2]), 3) << "\n"; }
+	if (check == "parallelepiped"){ std::cout << parseFloat(argv[2]) * parseFloat(argv[3]) * parseFloat(argv[4]) << "\n"; }
 
+	// Version
 	if (check == "version"){ std::cout << "Version: "<< Version << "\n"
 					      "Program created by: Samuel de Oliveira\n"; }
 
