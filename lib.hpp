8f06e603726ae942338a6bc5787b5a3201478d4f
Samuel-de-Oliveira <samwolfg13@gmail.com>
2022-11-01 20:02:24 -0300

    Put `helpMessage()` funtion to `lib.hpp`

--- lib.hpp
+++ lib.hpp
@@ -7,12 +7,37 @@
  *
  */
 
+#include <iostream>
 #include <sstream>
 
-const float Pi         {3.1415926535897}; // I conseidered this value for π.
+const float Pi        {3.1415926535897}; // I conseidered this value for π.
 const char Version[4] {"1.0"}; // Version's number.
 
-int parseInt(const std::string &str) {
+void helpMessage() {
+
+std::cout <<
+"\nTo use you need to follow this format:\n"
+"\teasarea [command] values...\n"
+"\n"
+"_*_ List of commands _*_\n"
+"\n"
+"rectangle: Calculate the area of a rectangle (b x h) [easarea rectangle [\"b\" value here] [\"h\" value here]]\n"
+"square: Calculate the area of a square (s²) [easarea square [s value here]]\n"
+"triangle: Calculate the area of a tringle ((b x h) / 2) [easarea triangle [\"b\" value here] [\"h\" value here]]\n"
+"circle: Calculate the area of a circle (πr²) [easarea circle [\"r\" value here]]\n"
+"trapezium: Calculate the area of a trapezium ((B + b) * h / 2) [easarea trapezium [\"B\" value here] [\"b\" value here] [\"h\" value here]]\n"
+"diamond: Calculate the area of a diamond ((D * d) / 2) [easarea diamond [\"D\" value here] [\"b\" value here]]\n"
+"hypotenuse: Calculate the hypotenuse of a triangle (X² = Y² + Z²) [easarea hypotenuse [\"Y\" value here] [\"Z\" value here]]\n"
+"regular_polygon: Calculate the area of a regular polygon ((P * A) / 2) [easarea regular_polygon [\"P\" value here] [\"A value here\"]]\n"
+"cube: Calculate the volume of a cube (s³) [easarea cube [\"s\" value here]]\n"
+"parallelepiped: Calculate the valume of a parallelepiped (b * h * w) [easarea parallelepiped [\"b\" value here] [\"h\" value here] [\"w\" value here]]\n"
+
+"version: Show the version of program [easarea version]\n"
+"help: Show this message [easarea help]\n\n";
+
+}
+
+int parseInt( const std::string &str ) {
 
 	std::stringstream ss(str);
 	int num;
@@ -21,7 +46,7 @@ int parseInt(const std::string &str) {
 
 }
 
-float parseFloat(const std::string &str) {
+float parseFloat( const std::string &str ) {
 
 	std::stringstream ss(str);
 	float num;
@@ -30,7 +55,7 @@ float parseFloat(const std::string &str) {
 
 }
 
-float circleParts(float angle) {
+float circleParts( const float angle ) {
 
 	if (angle > 360) {
 		std::cout << "The angle can't be a value higher than 360º.\n";
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
  *
  *  Crated by: Samuel de Oliveira (Samuel-de-Oliviera)
  *  Code contribuitors: None
+ *  Version: 1.0
  *
  */
 
@@ -16,32 +17,7 @@
 #include <string>
 #include <cmath>
 
-// Help message function
-void helpMessage(){
-
-std::cout <<
-"\nTo use you need to follow this format:\n"
-"\teasarea [command] values...\n"
-"\n"
-"_*_ List of commands _*_\n"
-"\n"
-"rectangle: Calculate the area of a rectangle (b x h) [easarea rectangle [\"b\" value here] [\"h\" value here]]\n"
-"square: Calculate the area of a square (s²) [easarea square [s value here]]\n"
-"triangle: Calculate the area of a tringle ((b x h) / 2) [easarea triangle [\"b\" value here] [\"h\" value here]]\n"
-"circle: Calculate the area of a circle (πr²) [easarea circle [\"r\" value here]]\n"
-"trapezium: Calculate the area of a trapezium ((B + b) * h / 2) [easarea trapezium [\"B\" value here] [\"b\" value here] [\"h\" value here]]\n"
-"diamond: Calculate the area of a diamond ((D * d) / 2) [easarea diamond [\"D\" value here] [\"b\" value here]]\n"
-"hypotenuse: Calculate the hypotenuse of a triangle (X² = Y² + Z²) [easarea hypotenuse [\"Y\" value here] [\"Z\" value here]]\n"
-"regular_polygon: Calculate the area of a regular polygon ((P * A) / 2) [easarea regular_polygon [\"P\" value here] [\"A value here\"]]\n"
-"cube: Calculate the volume of a cube (s³) [easarea cube [\"s\" value here]]\n"
-"parallelepiped: Calculate the valume of a parallelepiped (b * h * w) [easarea parallelepiped [\"b\" value here] [\"h\" value here] [\"w\" value here]]\n"
-
-"version: Show the version of program [easarea version]\n"
-"help: Show this message [easarea help]\n\n";
-
-}
-
-int main(int argc, char** argv) {
+int main( int argc, char** argv ) {
 	
 	// if digit only program name, it shows the help message.
 	if (argc == 1) helpMessage();
