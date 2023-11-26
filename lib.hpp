4713dcbc7f6b40f17cbefdb5867a7580628f4efb
Samuel-de-Oliveira <samwolfg13@gmail.com>
2023-11-26 12:54:22 +0000

    Colours!

--- lib.hpp
+++ lib.hpp
@@ -5,6 +5,8 @@
  * funtions to use in program,
  * enjoy it anyway.
  *
+ * Created by: Samuel-de-Oliveira.
+ *
  */
 
 // Import Area
@@ -13,31 +15,31 @@
 
 // Define the constants
 const float Pi         {3.1415926535897}; // I defined this value for π.
-const char Version[10] {"1.1.1"}; // Version's number.
+const char Version[6] {"1.1.1"}; // Version's number.
 
 // Help message
 void helpMessage() {
 
 std::cout <<
 "\nTo use you need to follow this format:\n"
-"\teasarea [command] values...\n"
+"\t\033[1measarea [command] values...\033[m\n"
 "\n"
 "_*_ List of commands _*_\n"
 "\n"
-"rectangle: Calculate the area of a rectangle (b x h) [easarea rectangle [\"b\" value here] [\"h\" value here]]\n\n"
-"square: Calculate the area of a square (s²) [easarea square [s value here]]\n\n"
-"triangle: Calculate the area of a tringle ((b x h) / 2) [easarea triangle [\"b\" value here] [\"h\" value here]]\n\n"
-"circle: Calculate the area of a circle (πr²) [easarea circle [\"r\" value here]]\n\n"
-"trapezium: Calculate the area of a trapezium ((B + b) * h / 2) [easarea trapezium [\"B\" value here] [\"b\" value here] [\"h\" value here]]\n\n"
-"diamond: Calculate the area of a diamond ((D * d) / 2) [easarea diamond [\"D\" value here] [\"b\" value here]]\n\n"
-"hypotenuse: Calculate the hypotenuse of a triangle (X² = Y² + Z²) [easarea hypotenuse [\"Y\" value here] [\"Z\" value here]]\n\n"
-"regular_polygon: Calculate the area of a regular polygon ((P * A) / 2) [easarea regular_polygon [\"P\" value here] [\"A value here\"]]\n\n"
-"cube: Calculate the volume of a cube (s³) [easarea cube [\"s\" value here]]\n\n"
-"parallelepiped: Calculate the valume of a parallelepiped (b * h * w) [easarea parallelepiped [\"b\" value here] [\"h\" value here] [\"w\" value here]]\n\n"
-"semicircle: Calculate the area of a semicircle ((r² * π) / (360 / angle)) [ easarea semicircle [\"r\" value]] [\"angle\" value]]\n\n"
+"\033[1;34mrectangle:\033[m Calculate the area of a rectangle (b x h) [easarea rectangle [\"b\" value here] [\"h\" value here]]\n\n"
+"\033[1;34msquare:\033[m Calculate the area of a square (s²) [easarea square [s value here]]\n\n"
+"\033[1;34mtriangle:\033[m Calculate the area of a tringle ((b x h) / 2) [easarea triangle [\"b\" value here] [\"h\" value here]]\n\n"
+"\033[1;34mcircle:\033[m Calculate the area of a circle (πr²) [easarea circle [\"r\" value here]]\n\n"
+"\033[1;34mtrapezium:\033[m Calculate the area of a trapezium ((B + b) * h / 2) [easarea trapezium [\"B\" value here] [\"b\" value here] [\"h\" value here]]\n\n"
+"\033[1;34mdiamond:\033[m Calculate the area of a diamond ((D * d) / 2) [easarea diamond [\"D\" value here] [\"b\" value here]]\n\n"
+"\033[1;34mhypotenuse:\033[m Calculate the hypotenuse of a triangle (X² = Y² + Z²) [easarea hypotenuse [\"Y\" value here] [\"Z\" value here]]\n\n"
+"\033[1;34mregular_polygon:\033[m Calculate the area of a regular polygon ((P * A) / 2) [easarea regular_polygon [\"P\" value here] [\"A value here\"]]\n\n"
+"\033[1;34mcube:\033[m Calculate the volume of a cube (s³) [easarea cube [\"s\" value here]]\n\n"
+"\033[1;34mparallelepiped:\033[m Calculate the valume of a parallelepiped (b * h * w) [easarea parallelepiped [\"b\" value here] [\"h\" value here] [\"w\" value here]]\n\n"
+"\033[1;34msemicircle:\033[m Calculate the area of a semicircle ((r² * π) / (360 / angle)) [ easarea semicircle [\"r\" value]] [\"angle\" value]]\n\n"
 
-"version: Show the version of program [easarea version]\n\n"
-"help: Show this message [easarea help]\n\n";
+"\033[1;34mversion:\033[m Show the version of program [easarea version]\n\n"
+"\033[1;34mhelp:\033[m Show this message [easarea help]\n\n";
 }
 
 // Parse interger
